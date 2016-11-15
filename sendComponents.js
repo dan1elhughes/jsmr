759f4451889e7d3103d3a1fc307aac3ba4188232
Dan Hughes <danhughes.dh+github@gmail.com>
2016-11-15 11:52:12 +0000

    Split out map and reduce functions for sending

--- sendComponents.js
+++ sendComponents.js
@@ -1,4 +1,6 @@
-module.exports = (socket, components) => Promise.all([
-	new Promise(resolve => socket.emit('data', components.data, resolve)),
-	new Promise(resolve => socket.emit('process', components.process.toString(), resolve)),
+module.exports = (socket, data, components) => Promise.all([
+	new Promise(resolve => socket.emit('data', data, resolve)),
+
+	new Promise(resolve => socket.emit('map', components.map.toString(), resolve)),
+	new Promise(resolve => socket.emit('reduce', components.reduce.toString(), resolve))
 ]);
