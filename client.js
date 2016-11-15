409c9398b2445258bd993f3bef2816ec123fedc3
Dan Hughes <danhughes.dh+github@gmail.com>
2016-11-15 11:51:52 +0000

    Add execution function to build and run JSVMs

--- client.js
+++ client.js
@@ -1,10 +1,21 @@
 const network = require('socket.io-client');
-const socket = network.connect('http://localhost:3000', {
-	reconnect: true
-});
+const socket = network.connect('http://localhost:3000', { reconnect: true });
 const os = require('os');
 const vm = require('vm');
 const console = require('util');
+
+const execute = process => () => {
+	let context = vm.createContext({
+		console,
+		data: components.data,
+		emit: data => socket.emit(`result-${process}`, data)
+	});
+
+	let result = vm.runInContext(components[process], context);
+	if (result) {
+		socket.emit(`result-${process}`, result);
+	}
+};
 
 socket.on('disconnect', () => console.log('Disconnected'));
 socket.on('connect', () => console.log('Connected'));
