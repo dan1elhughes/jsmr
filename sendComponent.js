012da0ca5545910def6c9a5abe7b9305a43c4652
Dan Hughes <danhughes.dh+github@gmail.com>
2016-11-16 16:59:22 +0000

    Use more meaningful variable name for queue length

--- sendComponent.js
+++ sendComponent.js
@@ -1,11 +1,11 @@
 const console = require('util');
 
-module.exports = (name, component) => (value, respond) => {
+module.exports = (name, component) => (quantity, respond) => {
 	if (component instanceof Function) {
 		component = component.toString();
 	}
 
-	let data = name === 'data' ? component.pop(value) : component;
+	let data = name === 'data' ? component.pop(quantity) : component;
 
 	console.log(`Sending ${name}`);
 	respond(data);
