ccead8f2eab547c7ebb7b606862f140581726f46
Dan Hughes <danhughes.dh+github@gmail.com>
2016-11-21 11:55:08 +0000

    Add queue.pop and queue.accumulate methods

--- Queue.js
+++ Queue.js
@@ -5,6 +5,34 @@ module.exports = function (data) {
 
 	this.length = () => this.queue.length;
 
+	/**
+	 * Pops the first item off the queue and all other items with the same key.
+	 * @return {Array} A collection of items with matching keys
+	 */
+	this.accumulate = () => {
+		let first = this.queue.pop();
+		let matches = [];
+		if (first) {
+			matches.push(first);
+			let key = first.key;
+
+			this.queue.forEach((element, i) => {
+				if (element.key === key) {
+					matches.push(this.pull(i));
+				}
+			});
+		}
+
+		return matches;
+	};
+
+	/**
+	 * Removes a specific item from the queue
+	 * @param  {Number} i Index of the item to remove
+	 * @return {Element} The element
+	 */
+	this.pull = i => this.queue.splice(i, 1)[0];
+
 	this.pop = count => {
 		count = count || 1;
 		let results = [];
@@ -14,8 +42,7 @@ module.exports = function (data) {
 
 			if (value) {
 				results.push(value);
-			} else if (this.queue.length === 0) {
-				results.push(null);
+			} else {
 				count = 0;
 			}
 		}
